63ef62a50ec15f0d5595cd7486ac059e95cb6505
Ghanshyam Mishra <ghanshyammishra3205615@gmail.com>
2022-12-25 22:15:51 +0530

    bugs fixed

--- app/src/main/java/com/example/quizania/DashboardActivity.java
+++ app/src/main/java/com/example/quizania/DashboardActivity.java
@@ -1,17 +1,25 @@
 package com.example.quizania;
 
+import static com.example.quizania.MainActivity.listOfQ;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.CollapsibleActionView;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.sasank.roundedhorizontalprogress.RoundedHorizontalProgressBar;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -20,13 +28,23 @@ public class DashboardActivity extends AppCompatActivity {
     CountDownTimer countDownTimer;
     int timerValue = 20;
     RoundedHorizontalProgressBar progressBar;
+    List<ModelClass> allQuestions;
+    ModelClass modelClass;
+    int index = 0;
+    TextView question, op1, op2, op3, op4;
+    CardView card1, card2, card3, card4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Hooks();
         setContentView(R.layout.activity_dashboard);
+        allQuestions = listOfQ;
+        Collections.shuffle(allQuestions);
+        modelClass = listOfQ.get(index);
 
-        progressBar = findViewById(R.id.progress_bar_1);
+        setAllData();
 
         countDownTimer = new CountDownTimer(20000, 1000) {
             @Override
@@ -51,5 +69,28 @@ public class DashboardActivity extends AppCompatActivity {
 
             }
         }.start();
+    }
+
+    private void setAllData() {
+        question.setText(modelClass.getQues());
+        op1.setText(modelClass.getAns1());
+        op2.setText(modelClass.getAns2());
+        op3.setText(modelClass.getAns3());
+        op4.setText(modelClass.getAns4());
+
+    }
+
+    private void Hooks() {
+        progressBar = findViewById(R.id.progress_bar_1);
+        question = findViewById(R.id.question);
+        op1 = findViewById(R.id.ans1);
+        op2 = findViewById(R.id.ans2);
+        op3 = findViewById(R.id.ans3);
+        op4 = findViewById(R.id.ans4);
+
+        card1 = findViewById(R.id.ans1_card);
+        card2 = findViewById(R.id.ans2_card);
+        card2 = findViewById(R.id.ans3_card);
+        card2 = findViewById(R.id.ans4_card);
     }
 }
--- app/src/main/java/com/example/quizania/MainActivity.java
+++ app/src/main/java/com/example/quizania/MainActivity.java
@@ -6,12 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static ArrayList<ModelClass> listOfQ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listOfQ = new ArrayList<>();
+        listOfQ.add(new ModelClass("what comes after 1", "2", "0", "2", "5", "2"));
+        listOfQ.add(new ModelClass("what comes after 2", "2", "0", "3", "5", "3"));
+        listOfQ.add(new ModelClass("what comes after 3", "2", "0", "4", "5", "4"));
+        listOfQ.add(new ModelClass("what comes after 4", "2", "0", "5", "5", "5"));
+        listOfQ.add(new ModelClass("what comes after 5", "2", "0", "6", "5", "6"));
+        listOfQ.add(new ModelClass("what comes after 6", "2", "0", "7", "5", "7"));
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
